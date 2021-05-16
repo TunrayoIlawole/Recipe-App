542c9f425f8007d16cf5a0a36ab73cd7fa01d989
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-05-16 12:08:02 +0100

    create daily recipe view

---
+++ Functions/displayDailyRecipe.js
@@ -0,0 +1,33 @@
+const domElements = {
+    dailyRecipe: document.querySelector('.recipe-con')
+}
+
+const displayDailyRecipe = (result) => {
+    const markup = `
+    <div class = "recipe-image">
+        <img src = "${result.image_url}" alt = "${result.title}" />
+    </div>
+    <h3>${result.title}</h3>
+    <div class = "recipe-details">
+        <div class="recipe-directions">
+            <h2 class="heading-2">How to cook it</h2>
+            <p class="recipe-directions-text">
+                This recipe was carefully designed and tested by
+                <span class="recipe-publisher">${result.publisher}</span>. Please check out
+                directions at their website.
+            </p>
+            <a
+                class="btn-small recipe-btn"
+                href="#"
+                target="_blank"
+            >
+                <span>Directions ➡️</span>
+            </a>
+        </div>
+    </div>`;
+
+
+    domElements.dailyRecipe.innerHTML = markup;
+}
+
+export default displayDailyRecipe;
