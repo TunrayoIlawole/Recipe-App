4a1f6a0c5802cd772a11e51712ed25d9425c7a9d
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-05-16 12:07:39 +0100

    daily recipe functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import getResults from './Functions/getResults';
 import displayResult from './Functions/displayResult';
+import displayDailyRecipe from './Functions/displayDailyRecipe';
 
 const domElements = {
     form: document.querySelector('.search'),
@@ -7,12 +8,42 @@ const domElements = {
     results: document.querySelector('.results'),
     loader: document.querySelector('.loader-con'),
     theCart: document.querySelector('.cart'),
-    heartIcon: document.querySelector('.heart')
+    heartIcon: document.querySelector('.heart'),
+    modal: document.querySelector('.modal'),
+    overlay: document.querySelector('.overlay'),
+    modalClose: document.querySelector('.close-modal'),
+    error: document.querySelector('.error')
 }
 
 const loader = `
 <div class = "loader"></div>
 `;
+
+
+function showDailyRecipe() {
+    const foods = ['pizza', 'fish', 'beef', 'pasta', 'steak', 'cake'];
+    const randomFood = foods[Math.trunc(Math.random() * foods.length)];
+    console.log(randomFood);
+
+    domElements.modal.classList.remove('hidden');
+    domElements.overlay.classList.remove('hidden');
+
+    try {
+        const results = getResults(randomFood);
+
+        results.then(response => {
+            const recipes = response.recipes;
+
+            const randomIndex = Math.trunc(Math.random() * recipes.length);
+            const randomRecipe = recipes[randomIndex];
+
+            console.log(randomRecipe);
+            displayDailyRecipe(randomRecipe);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 function handleSubmit(e) {
     e.preventDefault();
@@ -42,6 +73,7 @@ function handleSubmit(e) {
         })
     }
     catch (error) {
+
         console.log(error);
     }
 }
@@ -50,4 +82,11 @@ domElements.form.addEventListener('submit', handleSubmit);
 
 domElements.heartIcon.addEventListener('click', () => {
     domElements.theCart.classList.toggle('show');
+})
+
+window.addEventListener('load', showDailyRecipe);
+
+domElements.modalClose.addEventListener('click', () => {
+    domElements.modal.classList.add('hidden');
+    domElements.overlay.classList.add('hidden');
 })
