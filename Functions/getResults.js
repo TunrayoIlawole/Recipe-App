e29a3311f15b55ab0d2ecfcd2e47f4404b9c2efb
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-05-17 17:45:05 +0100

    add error handler

--- Functions/getResults.js
+++ Functions/getResults.js
@@ -9,6 +9,15 @@ function getResults(searchQuery) {
     .then(response => {
         return response.data;
     })
+    .catch(error => {
+        const loader = document.querySelector('.loader');
+
+        if (loader) {
+            loader.parentElement.removeChild(loader);
+        }
+
+        document.querySelector('.error').classList.remove('hidden');
+    })
 }
 
 export default getResults;
